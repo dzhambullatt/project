1f394710d3429188e7da3d8a9512514495c0e503
djambulat <jambulatahmadov99@gmail.com>
2022-11-14 20:13:51 +0300

    created new templates

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return "hello about"
+    return render_template("about.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "user name: " + name + " - " + str(id)
 
 if __name__ == '__main__':
     app.run()
